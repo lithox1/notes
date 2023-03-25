156392b9ff3322aa2bcbec490e9c71ad73da42db
lithox1 <102911465+lithox1@users.noreply.github.com>
2023-03-25 10:59:46 +0100

    Test

--- backend/index.cjs
+++ backend/index.cjs
@@ -1,10 +1,10 @@
-const express = require("express")()
+const express = require("express")
 const app = express()
 const cors = require("cors")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("public"))
+app.use(express.static("dist"))
 
 let notes = [
   {
@@ -76,5 +76,3 @@ app.delete("/api/notes/:id", (req, res) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
-
-module.exports = app
